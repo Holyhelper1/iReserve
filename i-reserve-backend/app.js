7ba931ea3c89068dc8a7dc8be4553688cb59063c
Alexander Shulgin <shulgin.alexandera@gmail.com>
2024-08-12 23:07:02 +0300

    fixed api path

--- i-reserve-backend/app.js
+++ i-reserve-backend/app.js
@@ -13,10 +13,20 @@ app.use(express.static("../i-reserve/build"));
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/", routes);
+app.use("/api", routes);
 
-mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
-  app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("..", "i-reserve", "build", "index.html")) 
 });
+
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  });
+
